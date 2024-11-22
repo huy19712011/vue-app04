afda0287df79ee83d515e8594db7d879fbbb5167
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2024-11-22 23:30:01 +0700

    Tutorial of JeffreyWay: set up, hello world

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from "vue";
+import { RouterLink, RouterView } from "vue-router";
+const greeting = ref("Hello World");
+import { onMounted } from "vue";
+
+onMounted(() => {
+  console.log("Component is mounted");
+  setTimeout(() => {
+    greeting.value = "Changed";
+  }, 3000);
+});
+</script>
+
+<template>
+  <p>
+    <input
+      type="text"
+      v-model="greeting"
+    />
+  </p>
+  <p>{{ greeting }} ({{ greeting.length }})</p>
+</template>
