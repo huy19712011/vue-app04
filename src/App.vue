6538f76a80e09f0ebf8646775c0a883290ba50ec
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2024-11-23 00:38:37 +0700

    v3: Lists, Conditionals, Computed properties

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,64 @@
 <script setup>
-import { ref } from "vue";
+import { computed, ref } from "vue";
 import { RouterLink, RouterView } from "vue-router";
 import { onMounted } from "vue";
 
-const buttonClasses = ref(["text-green"]);
-const active = ref(false);
-const toggle = () => {
-  // buttonClasses.value = "text-red";
-  active.value = !active.value;
-};
+const assignments = ref([
+  { id: 1, name: "Finish project", complete: false },
+  { id: 2, name: "Read chapter 4", complete: false },
+  { id: 3, name: "Turn in homework", complete: false },
+]);
+
+const completedAssignments = computed(() => {
+  return assignments.value.filter((assignment) => assignment.complete);
+});
+const inProgressAssignments = computed(() => {
+  return assignments.value.filter((assignment) => !assignment.complete);
+});
 </script>
 
 <template>
   <div class="center">
-    <button
-      :class="active ? 'text-red' : 'text-green'"
-      @click="toggle"
-    >
-      Click me
-    </button>
+    <section v-show="inProgressAssignments.length">
+      <h2>In Progress</h2>
+      <ul>
+        <li
+          v-for="assignment in inProgressAssignments"
+          :key="assignment.id"
+        >
+          <label>
+            {{ assignment.name }}
+            <input
+              type="checkbox"
+              v-model="assignment.complete"
+            />
+          </label>
+        </li>
+      </ul>
+      <!-- <pre>
+        {{ assignments }}
+      </pre> -->
+    </section>
+    <section v-show="completedAssignments.length">
+      <h2>Completed</h2>
+      <ul>
+        <li
+          v-for="assignment in completedAssignments"
+          :key="assignment.id"
+        >
+          <label>
+            {{ assignment.name }}
+            <input
+              type="checkbox"
+              v-model="assignment.complete"
+            />
+          </label>
+        </li>
+      </ul>
+      <!-- <pre>
+        {{ assignments }}
+      </pre> -->
+    </section>
   </div>
 </template>
 
